099d4e4bb071fc7f3593b74b006895ec8617e5b6
Roman Ryzhiy <rom.rzh@gmail.com>
2017-01-26 04:00:25 +0200

    update_2

--- static_ip.py
+++ static_ip.py
@@ -1,67 +1,112 @@
 #!flask/bin/python
-from flask import Flask, jsonify, request, abort
-import pexpect
+from flask import Flask, jsonify
+import  os
+from jinja2 import Template
+
 
 app = Flask(__name__)
 
-#---------------------------------------------------------------------------------#
-def restart_dhcp():
-    child = pexpect.spawn('sudo /etc/init.d/isc-dhcp-server restart')
-    t = child.expect(['Restarting', pexpect.EOF, pexpect.TIMEOUT])
-    if t == 0 :
-       child.sendline('supermen23')
-       t = child.expect(['[ok]', pexpect.EOF, pexpect.TIMEOUT])
-       if t == 0:
-          return True
-       else:
-          return False
-#--------------------------------------------------------------------------------#    
-    
-    
+template_conf = Template(u"""
+host {{login}}
+{
+hardware ethernet {{MAC}};
+fixed-address {{IP}};
+}
+
+""")
+
+
+
 @app.route('/add/<login>/<MAC>/<IP>')
 def add(login,MAC,IP):
-    f=open('static_ip.conf','r')
-    l = [line.strip() for line in f]
-    f.close()
-    if 'fixed-address '+IP+';' in l and l.index('fixed-address '+IP+';') != l.index('host '+login+' {')+2 :
-	return jsonify({'Error': 'IP is already used'})
-    if 'hardware ethernet '+MAC+';' in l and l.index('hardware ethernet '+MAC+';') != l.index('host '+login+' {')+1 :
-	return jsonify({'Error': 'MAC is already used'})
-    if "host "+login+" {" in l :
-       del l[l.index("host "+login+" {"):l.index("host "+login+" {")+4]
-       f=open('static_ip.conf','w')
-       for index in l:
-           f.write(index+'\n')
-       f.close()	
-
-    config=open('static_ip.conf','a')
-    config.write("host "+login+" {\n hardware ethernet "+MAC+";\n fixed-address "+IP+";\n}\n")
-    config.close()
+    open_config()
+    for i in range(0,len(parameters)) :
+	if IP in parameters[i][2] and parameters[i][0] != login :
+	    host_IP = parameters[i][0]
+            return response("error","IP is already used by "+host_IP)
+	    
+        elif MAC in parameters[i][1] and parameters[i][0] != login :
+            host_MAC = parameters[i][0]
+            return response("error","MAC is already used by "+host_MAC)
+    
+    for i in range(0,len(config)) :
+        if config[i]["login"] == login :
+	    del config[i]
+            
+    config.append({"login":login,"MAC":MAC,"IP":IP})
+  
+    with open('static_ip.conf','w') as f:
+        for i in range(0,len(config)):
+            f.write(template_conf.render(config[i]))
 
     if restart_dhcp():
-      return jsonify({'Added':'ok'}),201
-    else:
-      return jsonify({'error':"can't restart dhcp"}),500
+        return response("succsess","added")
+        
+    else :
+        return response("error","can't restart dhcp")
+        
+
+
 
 @app.route('/delete/<login>')
 def delete(login):
-    f=open('static_ip.conf','r')
-    l = [line.strip() for line in f]
-    f.close()
+    open_config()
+    for i in range(0,len(config)) :
+        if config[i]["login"] == login :
+	    del config[i]
+            with open('static_ip.conf','w') as f:
+                for i in range(0,len(config)):
+                    f.write(template_conf.render(config[i]))
+
+            if restart_dhcp():
+               return response("success","deleted")
+              
+            else :
+               return response("error","can't restart dhcp")
+               
+    return response("error","not found login in config")
     
-    if "host "+login+" {" in l:
-       del l[l.index("host "+login+" {"):l.index("host "+login+" {")+4]
-       f=open('static_ip.conf','w')
-       for index in l:
-           f.write(index+'\n')
-       f.close()
-    else:
-       return jsonify({"error":"Not found"}),404
-    if restart_dhcp():
-       return jsonify({"status":"deleted"}),200
-    else:
-       return jsonify({"error":"can't restart dhcp"}),500
-   
+
+@app.route('/show/<login>')
+def show(login) :
+    open_config()
+    if login != "all" :
+       for i in range(0,len(config)) :
+           if config[i]["login"] == login :
+               return response("success",login+" config",config[i])
+               
+       return response("error","not found login in config")
+    else :
+       return response("success","all config",config)
+       
+    
+      
+
+def open_config():
+    global config, parameters
+    parameters = []
+    config = []
+    with open('static_ip.conf','r') as l:
+	l = [line.strip() for line in l]	
+	for i in range(0,len(l),6):
+            parameters.append([l[i+1].split()[1],l[i+3].split()[2].replace(";",""),l[i+4].split()[1].replace(";","")])
+        for i in range(0,len(parameters)) :
+            config.append({"login":parameters[i][0] , "MAC":parameters[i][1] , "IP":parameters[i][2]})
+
+
+def restart_dhcp():
+    returncode = os.system('/etc/init.d/isc-dhcp-server restart')
+    if returncode == 0:
+       return True
+    else :
+       return False
+
+def response(stat,mass,dat=None) :
+    if dat == None :
+        response = jsonify(stastus=stat,massage=mass)
+    else :
+        response = jsonify(stastus=stat,massage=mass,data=dat)
+    return (response)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=1992, debug=True)
